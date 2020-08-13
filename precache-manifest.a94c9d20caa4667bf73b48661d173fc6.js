5d75448532474190ece07ceec452f955fd1e1150
Da-niel-G <da-niel-g.github.com>
2020-08-14 00:14:16 +0500

    songbird

---
+++ precache-manifest.a94c9d20caa4667bf73b48661d173fc6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193f9bd95559a9d7dc5cfd817cbf7ef2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23b2776774be239c8550",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "403678da8fede0f678b8",
+    "url": "/static/css/main.4dd1d121.chunk.css"
+  },
+  {
+    "revision": "23b2776774be239c8550",
+    "url": "/static/js/2.fa89f1ea.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.fa89f1ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "403678da8fede0f678b8",
+    "url": "/static/js/main.e66ebf77.chunk.js"
+  },
+  {
+    "revision": "dc9173b7e8edec60da3a",
+    "url": "/static/js/runtime-main.3ffde9c1.js"
+  },
+  {
+    "revision": "2de2ffb07fe2251464f4d416ca7fff04",
+    "url": "/static/media/no-name-animals.2de2ffb0.png"
+  },
+  {
+    "revision": "feccf697a3f89224954c19f0d366764e",
+    "url": "/static/media/songanimal-logo.feccf697.png"
+  },
+  {
+    "revision": "f87ce96bead6dcc3d66d541df40c5949",
+    "url": "/static/media/sound-right.f87ce96b.mp3"
+  },
+  {
+    "revision": "3e8db2155c14af86788c15d528e5596e",
+    "url": "/static/media/sound-wrong.3e8db215.mp3"
+  }
+]);
